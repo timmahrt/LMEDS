a4f15fb003e3532e6069a685f3099058141cd35f
timmahrt <timmahrt@gmail.com>
2015-08-11 23:38:22 -0500

    FEATURE: Script for post-processing experiment data
    Fully extracts and collates surveys and rpt data.  Extracts
    but does not collate everything else.

---
+++ lmeds/user_scripts/post_process_results.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+'''
+Created on Aug 9, 2015
+
+@author: timmahrt
+'''
+
+import os
+from os.path import join
+import sys
+import argparse
+import shutil
+import codecs
+
+from os.path import dirname, abspath
+
+os.chdir(dirname(dirname(abspath(__file__))))
+sys.path.append("..")
+
+from lmeds.io import sequence
+from lmeds.io import user_response
+from lmeds.io import survey
+from lmeds.io import loader
+from lmeds.utilities import utils
+from lmeds.utilities import user_script_helper
+from lmeds.utilities import constants
+from lmeds.post_process import transpose_rpt
+from lmeds.post_process import transpose_survey
+
+ASPECT_MEANING = "meaning"
+ASPECT_ACOUSTICS = "acoustics"
+ASPECT_WRITING = "writing"
+ASPECT_IMAGINE = "imagine"
+
+
+def extractFromTest(path, keyList, removeItemList=None, onlyKeepList=None):
+    '''
+    Extracts all matching keys from a user's results in an LMEDS test
+    '''
+    
+    # Load all user data
+    # -- separate the command name on each line from the rest of the line
+    testSequenceDataList = []
+    for fn in utils.findFiles(path, filterExt=".csv"):
+        with open(join(path, fn), "Ur") as fd:
+            subjectDataList = fd.read().split("\n")
+        subjectDataList = [line.split(",", 1) for line in subjectDataList]
+        testSequenceDataList.append((fn, subjectDataList))
+     
+    for key in keyList:
+        
+        outputDir = join(path, key)
+        utils.makeDir(outputDir)
+         
+        for fn, subjectDataList in testSequenceDataList:
+            subjectDataSubsetList = []
+            for line in subjectDataList:
+                command = line[0]
+                if command == key:
+                    partialLine = line[1].split(';,')[0]
+                    
+                    skipOut = False
+                    if removeItemList is not None:
+                        for item in removeItemList:
+                            if item in partialLine:
+                                skipOut = True
+                    if onlyKeepList is not None:
+                        for item in onlyKeepList:
+                            if item not in partialLine:
+                                skipOut = True
+                    if skipOut is True:
+                        continue
+                    subjectDataSubsetList.append(",".join(line))
+            # Don't output a blank file
+            if subjectDataSubsetList == []:
+                continue
+            
+            open(join(outputDir, fn),
+                 "w").write("\n".join(subjectDataSubsetList))
+
+
+def removeDuplicates(path, overwrite=False):
+
+    outputPath = join(path, "duplicates_removed")
+    if overwrite is False:
+        os.mkdir(outputPath)
+    else:
+        utils.makeDir(outputPath)
+    
+    anyDuplicatesFound = False
+    
+    for fn in utils.findFiles(path, filterExt=".csv"):
+        
+        data = open(join(path, fn), "r").read()
+        dataList = data.splitlines()
+        
+        outputList = [dataList[0], ]
+        prevString = dataList[0].rsplit("]", 1)[0]
+        for i in xrange(1, len(dataList)):
+            curString = dataList[i].rsplit("]", 1)[0]
+            
+            if curString == prevString:
+                if anyDuplicatesFound is False:
+                    print("Duplicates removed:")
+                    anyDuplicatesFound = True
+                print("%s, %d, %s" % (fn, i, curString))
+            else:
+                outputList.append(dataList[i])
+            
+            prevString = curString
+        
+        open(join(outputPath, fn), "w").write("\n".join(outputList))
+
+    print("End of duplicates listing")
+    
+
+def agglutinateSpreadsheets(csvFNList, outputFN):
+    
+    csvDataList = [codecs.open(fn, "r", encoding="utf-8").readlines()
+                   for fn in csvFNList]
+    
+    outputDataList = []
+    for rowList in utils.safeZip(csvDataList, enforceLength=True):
+        rowList = [row.replace("\n", "") for row in rowList]
+        outputDataList.append(",".join(rowList))
+        
+    outputTxt = "\n".join(outputDataList) + "\n"
+    codecs.open(outputFN, "w", encoding="utf-8").write(outputTxt)
+
+
+def postProcessResults(testName, sequenceFN, removeDuplicatesFlag,
+                       removeItemList=None):
+    
+    rootPath = join(constants.rootDir, "tests", testName)
+    txtPath = join(rootPath, "txt")
+    tmpSequence = sequence.TestSequence(None, join(rootPath, sequenceFN))
+    fullPath = join(rootPath, "output", tmpSequence.sequenceTitle)
+    pathToData = fullPath
+    outputPath = join(fullPath, "post_process_results")
+    
+    if removeDuplicatesFlag is True:
+        removeDuplicates(pathToData, True)
+        pathToData = join(pathToData, "duplicates_removed")
+    
+    userResponseList = []
+    fnList = utils.findFiles(pathToData, filterExt=".csv")
+    for fn in fnList:
+        fullPath = join(pathToData, fn)
+        userResponseList.append(user_response.loadUserResponse(fullPath))
+    
+    # Don't continue if files are of different lengths
+    testLen = len(userResponseList[0])
+    if not all([len(response) == testLen for response in userResponseList]):
+        print("ERROR: Not all responses in folder %s are the same length"
+              % pathToData)
+        for fn, response in utils.safeZip([fnList, userResponseList], True):
+            print("%s - %d lines" % (fn, len(response)))
+        exit(0)
+    
+    # Don't continue if pages are different
+    pageNameList = [[(pageTuple[0], pageTuple[1]) for pageTuple in response]
+                    for response in userResponseList]
+    sameList = []
+    fnListOfLists = []
+    for fn, pageList in utils.safeZip([fnList, pageNameList], True):
+        i = 0
+        while True:
+            if len(sameList) == i:
+                sameList.append(pageList)
+                fnListOfLists.append([])
+            else:
+                if sameList[i] == pageList:
+                    fnListOfLists[i].append(fn)
+                    break
+                else:
+                    i += 1
+    
+    if len(sameList) == 0:
+        print("ERROR: There don't appear to be any test data in folder %s"
+              % pathToData)
+        exit(0)
+        
+    if len(sameList) != 1:
+        print("ERROR: User data doesn't agree.  Filenames printed on "
+              "different lines differ in their pages.")
+        
+        for subFNList in fnListOfLists:
+            print(", ".join(subFNList))
+            
+    # Extract the different tests users completed
+    uniquePageList = []
+    for pageTuple in pageNameList[0]:
+        pageName = pageTuple[0]
+        if pageName not in uniquePageList:
+            uniquePageList.append(pageName)
+    
+    extractFromTest(pathToData, uniquePageList, removeItemList)
+    
+    # Transpose the surveys
+    if "survey" in uniquePageList:
+        surveyNameList = []
+        for pageName, stimuliArgList in pageNameList[0]:
+            if pageName == "survey":
+                surveyName = stimuliArgList[0]
+                surveyNameList.append(join(rootPath, surveyName + '.txt'))
+        
+        transpose_survey.transposeSurvey(join(pathToData, "survey"),
+                                         surveyNameList, outputPath)
+    
+    # Transpose the rpt pages
+    prominencePageList = ["prominence", "boundary", "boundary_and_prominence"]
+    for pageName in prominencePageList:
+        if pageName in uniquePageList:
+            transpose_rpt.transposeRPT(join(pathToData, pageName),
+                                       txtPath, pageName, outputPath)
+    
+
+if __name__ == "__main__":
+
+    description = "Verifies that the sequence file is well formed"
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('test_name', action='store',
+                        help=("The name of the root folder that stores the "
+                              "test files"))
+    parser.add_argument('sequence_fn', action='store',
+                        help=("The sequence .txt file"))
+    parser.add_argument('remove_duplicates_flag', action='store',
+                        help=("Remove duplicate items?  (true/false)"))
+    parser.add_argument('--remove_list', action='store', nargs='*',
+                        help=("A list of stimuli names to remove from "
+                              "consideration.  Typically, these are testing "
+                              "or demonstration items.  List as many items "
+                              "as needed after --remove_list, separated by "
+                              "a space character"))
+    try:
+        cmdArgs = user_script_helper.runScriptLogic(parser)
+    except user_script_helper.InteractiveModeException:
+        _test_name = raw_input(("Enter the name of your test "
+                                "(the root folder):\n"))
+        _sequence_fn = raw_input("Enter the name of your sequence file:\n")
+        _remove_duplicates_flag = raw_input("Remove duplicate items? "
+                                            "(true/false)\n")
+        _remove_items_flag = raw_input("Would you like to remove any items "
+                                       "(e.g. sample/test items)? (yes/no):\n")
+        if _remove_items_flag.lower() == 'yes':
+            _remove_item_list = raw_input("Enter a list of stimuli names to "
+                                          "remove (separated by a space):\n")
+            _remove_item_list = _remove_item_list.split(" ")
+        else:
+            _remove_item_list = []
+    else:
+        _test_name = cmdArgs.test_name
+        _sequence_fn = cmdArgs.sequence_fn
+        _remove_duplicates_flag = cmdArgs.remove_duplicates_flag
+        _remove_item_list = cmdArgs.remove_list
+    
+    if _remove_duplicates_flag.lower() == 'true':
+        _remove_duplicates_flag = True
+    elif _remove_duplicates_flag.lower() == 'false':
+        _remove_duplicates_flag = False
+
+    postProcessResults(_test_name, _sequence_fn, _remove_duplicates_flag,
+                       _remove_item_list)
